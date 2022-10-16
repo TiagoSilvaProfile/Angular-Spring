7f3ae37f1641105a28baba5db2a694ca8dad1af5
TiagoSilvaProfile <tiagoantonio8244@gmail.com>
2022-10-15 23:25:13 -0300

    Criação de Aplicação CRUD Angular + Spring

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,22 @@
+import { CoursesService } from './../service/courses.service';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [];
+  displayedColumns = ['name','category'];
+
+  constructor(private coursesService:CoursesService) { }
+
+  ngOnInit(): void {
+    this.courses = this.coursesService.list();
+
+  }
+
+}
